06cb9ca427ef011caadaa8d790b07e788fcf8a64
ashwinigit123 <ashwinitrale8@gmail.com>
2024-09-21 23:31:18 -0500

    change in main.py cors code

--- backend/main.py
+++ backend/main.py
@@ -50,7 +50,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Add your frontend URL
+    allow_origins=["*"],  # Add your frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
